14860fbb4f8003a379b5271e4a2c98345a82b6f0
Jeremiah Risinger <risinger.jeremiah@gmail.com>
2024-02-23 09:49:42 -0600

    Title Component

--- AngularComponentHierarchy/src/app/app.module.ts
+++ AngularComponentHierarchy/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ButtonComponent } from './components/button/button.component';
 import { ButtonRowComponent } from './components/button-row/button-row.component';
+import { TitleComponent } from './components/title/title.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ButtonComponent,
-    ButtonRowComponent
+    ButtonRowComponent,
+    TitleComponent
   ],
   imports: [
     BrowserModule
